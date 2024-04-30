c59ce1dfebeec26ed1ac283ed092f6b6dd532879
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-04-30 12:45:58 +0200

    Add auto yes

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 mamba create -y -n 04_regression python=3.10
 mamba activate 04_regression
-mamba install pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
+mamba install -y pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install ipykernel "careamics[examples,tensorboard] @ git+https://github.com/CAREamics/careamics.git"
