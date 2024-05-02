dfaaa557fe98e6b4c7a5489a67f447d2bd8c285a
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2024-05-02 16:06:32 +0200

    Copy file from prev exercise

--- setup.sh
+++ setup.sh
@@ -3,22 +3,28 @@
 # source the mamba installation to be able to activate the env
 source /localscratch/miniforge3/etc/profile.d/conda.sh
 
+# script directory (used to make sure the relative paths are fine)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # create environment
-env="02_regression"
-mamba create -y -n "$env" python=3.10
-conda activate "$env"
+ENV="02_regression"
+mamba create -y -n "$ENV" python=3.10
+conda activate "$ENV"
 
 # check that the environment was activated
-if [[ "$CONDA_DEFAULT_ENV" == "$env" ]]; then
+if [[ "$CONDA_DEFAULT_ENV" == "$ENV" ]]; then
     echo "Environment activated successfully"
 else
     echo "Failed to activate the environment"
 fi
 
 # Further instructions that should only run if the environment is active
-if [[ "$CONDA_DEFAULT_ENV" == "$env" ]]; then
+if [[ "$CONDA_DEFAULT_ENV" == "$ENV" ]]; then
     mamba install -y pytorch-gpu cuda-toolkit=11.8 torchvision -c nvidia -c conda-forge -c pytorch
     #mamba install -y pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
     pip install ipykernel "careamics[examples,tensorboard] @ git+https://github.com/CAREamics/careamics.git"
 
+    # copy files from the other repo for the CARE exercise
+    cd "$SCRIPT_DIR"
+    cp ../01_segmentation/unet.py CARE/
 fi
